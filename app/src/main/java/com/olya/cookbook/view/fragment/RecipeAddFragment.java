4e1ed0412a7e33c5acd4cc531f204c00a6af808c
olya-morozova <olya-morozova@users.noreply.github.com>
2017-10-07 23:49:52 -0600

    Added choosing image for recipe from gallery

--- app/src/main/java/com/olya/cookbook/view/fragment/RecipeAddFragment.java
+++ app/src/main/java/com/olya/cookbook/view/fragment/RecipeAddFragment.java
@@ -1,18 +1,33 @@
 package com.olya.cookbook.view.fragment;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.olya.cookbook.R;
+
+import java.io.InputStream;
 
 /**
  * Created by Olya on 21/09/2017.
  */
 
 public class RecipeAddFragment extends Fragment {
+
+    public static final int PICK_IMAGE = 1;
+
+    View view;
+    ImageView recipeImage;
 
     public RecipeAddFragment() {
         // Required empty public constructor
@@ -21,8 +36,47 @@ public class RecipeAddFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_res_add, container, false);
+        recipeImage = view.findViewById(R.id.recipeImage);
 
-        View view = inflater.inflate(R.layout.fragment_res_add, container, false);
+        recipeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+            }
+        });
+
         return view ;
     }
+
+    public void pickImage() {
+        Intent intent = new Intent(
+                Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+    }
+/*
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE && resultCode == Activity.RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = this.getActivity().getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+
+            recipeImage.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        }
+    }
+*/
 }
