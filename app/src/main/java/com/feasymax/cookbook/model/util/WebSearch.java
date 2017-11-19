6d434328658537fb6de6f2498e9f575d145bf157
olya-morozova <olya-morozova@users.noreply.github.com>
2017-11-19 14:59:41 -0600

    Added jsoup library for websearch and started working on it

--- app/src/main/java/com/feasymax/cookbook/model/util/WebSearch.java
+++ app/src/main/java/com/feasymax/cookbook/model/util/WebSearch.java
@@ -1,11 +1,56 @@
 package com.feasymax.cookbook.model.util;
 
+import android.util.Log;
+
+import com.feasymax.cookbook.view.list.RecipeListModel;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
  * Created by Olya on 2017-10-12.
  */
 
 public class WebSearch {
-    public WebSearch() {
+
+    private WebSearch() {}
+
+    public static List<RecipeListModel> getSearchResults(String searchString){
+        // TODO: tokenize searchString or simply substitute delimiters but '+' to use the
+        // resulting string for the address in connect()
+
+        Document doc;
+        try{
+            doc = Jsoup.connect("https://www.google.ca/search?q=bread+recipe").userAgent("Mozilla").ignoreHttpErrors(true).timeout(0).get();
+            Elements links = doc.select("div[class=g]");
+            for (Element link : links) {
+                Elements titles = link.select("h3[class=r]");
+                String title = titles.text();
+
+                Elements bodies = link.select("span[class=st]");
+                String body = bodies.text();
+
+                Elements addrs = link.select("a[href]");
+                String addr = addrs.attr("abs:href");
+
+                // TODO: also save an image
+
+                Log.println(Log.INFO, "getSearchResults", "Title: " + title);
+                Log.println(Log.INFO, "getSearchResults", "Body: "+body);
+                Log.println(Log.INFO, "getSearchResults", "Link: "+addr+"\n");
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // TODO: create a LinkedList<LinkInfo>, put every link info there and return it
+        return null;
     }
 
 
--- app/src/main/java/com/feasymax/cookbook/view/fragment/DiscoverWebsearchFragment.java
+++ app/src/main/java/com/feasymax/cookbook/view/fragment/DiscoverWebsearchFragment.java
@@ -2,17 +2,21 @@ package com.feasymax.cookbook.view.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.feasymax.cookbook.R;
+import com.feasymax.cookbook.model.util.WebSearch;
 
 /**
  * Created by Olya on 2017-09-21.
  */
 
 public class DiscoverWebsearchFragment extends Fragment {
+
+    android.widget.SearchView searchView;
 
     public DiscoverWebsearchFragment() {
         // Required empty public constructor
@@ -22,6 +26,23 @@ public class DiscoverWebsearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_dis_websearch, container, false);
+
+        searchView = view.findViewById(R.id.searchView);
+        /*
+        searchView.setOnQueryTextListener(new android.widget.SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                WebSearch.getSearchResults(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        */
+
         return view ;
     }
 }
