d2022295d1496131d2f2ed897f263b47e9399d96
Claviger <claviger@vivaldi.net>
2017-11-21 17:55:07 -0600

    Added conversionFactors and file reading for it. Trying to piece together local storage for Concersion Factors. Incomplete as of now.

---
+++ app/src/main/java/com/feasymax/cookbook/model/entity/ConversionFactor.java
@@ -0,0 +1,16 @@
+package com.feasymax.cookbook.model.entity;
+
+/**
+ * Created by CLAVIGER on 2017-11-21.
+ */
+
+public class ConversionFactor {
+    //Personal Class for file parsing
+    public String name;
+    public double factor;
+
+    public ConversionFactor(String nameC, double factorC) {
+        name = nameC;
+        factor = factorC;
+    }
+}
